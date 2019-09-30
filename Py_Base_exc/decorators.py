90c9d784d57fff1a759ee901d559767d09e31d7b
guozhicheng46 <467563369@qq.com>
2019-09-30 16:12:03 +0800

    930基础复习-1

---
+++ Py_Base_exc/decorators.py
@@ -0,0 +1,157 @@
+'''
+def hi(name="yasoob"):
+    return "hi " + name
+
+
+print(hi())
+# output: 'hi yasoob'
+
+# 我们甚至可以将一个函数赋值给一个变量，比如
+greet = hi
+# 我们这里没有在使用小括号，因为我们并不是在调用hi函数
+# 而是在将它放在greet变量里头。我们尝试运行下这个
+
+print(greet())
+# output: 'hi yasoob'
+
+# 如果我们删掉旧的hi函数，看看会发生什么！
+del hi
+print(hi())
+# outputs: NameError
+
+print(greet())
+# outputs: 'hi yasoob'
+'''
+
+'''
+def hi(name="yasoob"):
+    print("now you are inside the hi() function")
+
+    def greet():
+        return "now you are in the greet() function"
+
+    def welcome():
+        return  "now you are in the welcome() function"
+
+    print(greet())
+    print(welcome())
+    print("now you are back in the hi() function")
+
+hi()
+# output:now you are inside the hi() function
+#       now you are in the greet() function
+#       now you are in the welcome() function
+#       now you are back in the hi() function
+
+# 上面展示了无论何时你调用hi(), greet()和welcome()将会同时被调用。
+# 然后greet()和welcome()函数在hi()函数之外是不能访问的，比如：
+
+#greet()
+# outputs: NameError: name 'greet' is not defined
+'''
+
+'''
+def hi(name="yasoob"):
+    def greet():
+        return "now you are in the greet() function"
+
+    def welcome():
+        return "now you are in the welcome() function"
+
+    if name=='yasoob':
+        return greet
+    else:
+        return welcome
+
+a=hi()
+print(a)
+# outputs: <function greet at 0x7f2143c01500>
+
+# 上面清晰地展示了`a`现在指向到hi()函数中的greet()函数
+# 现在试试这个
+print(a())
+# outputs: now you are in the greet() function
+print(hi(123)())
+# now you are in the welcome() function
+'''
+
+'''
+def hi():
+    return "hi yasoob!"
+
+def doSomethingBeforeHi(func):
+    print("I am doing some boring work before executing hi()")
+    print(func())
+
+doSomethingBeforeHi(hi)
+#outputs:I am doing some boring work before executing hi()
+#        hi yasoob!
+'''
+
+'''
+def a_new_decorator(a_func):
+    def wrapTheFunction():
+        print("I am doing some boring work before executing a_func()")
+
+        a_func()
+
+        print("I am doing some boring work after executing a_func()")
+
+    return wrapTheFunction
+
+
+def a_function_requiring_decoration():
+    print("I am the function which needs some decoration to remove my foul smell")
+
+
+a_function_requiring_decoration()
+# outputs: "I am the function which needs some decoration to remove my foul smell"
+
+a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
+# now a_function_requiring_decoration is wrapped by wrapTheFunction()
+
+a_function_requiring_decoration()
+# outputs:I am doing some boring work before executing a_func()
+#        I am the function which needs some decoration to remove my foul smell
+#        I am doing some boring work after executing a_func()
+
+@a_new_decorator
+def a_function_requiring_decoration():
+    """Hey you! Decorate me!"""
+    print("I am the function which needs some decoration to "
+          "remove my foul smell")
+
+
+a_function_requiring_decoration()
+# outputs: I am doing some boring work before executing a_func()
+#         I am the function which needs some decoration to remove my foul smell
+#         I am doing some boring work after executing a_func()
+
+# the @a_new_decorator is just a short way of saying:
+# a_function_requiring_decoration = a_new_decorator(a_function_requiring_decoration)
+'''
+from functools import wraps
+
+
+def decorator_name(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if not can_run:
+            return "Function will not run"
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+@decorator_name
+def func():
+    return ("Function is running")
+
+
+can_run = True
+print(func())
+# Output: Function is running
+
+can_run = False
+print(func())
+# Output: Function will not run
