f71cd33f479fe92f5d4121a3aab19783b184c3d9
guozhicheng46 <467563369@qq.com>
2019-10-29 20:20:36 +0800

    1029appium复习-1

--- page_object/testcases/test_hysapp.py
+++ page_object/testcases/test_hysapp.py
@@ -1,45 +1,56 @@
 from appium import webdriver
 import time
 
-def install_app():
-    caps={}
-    caps["platformName"] = "android"
-    caps["deviceName"] = "GT_New"
-    caps["appPackage"] = "com.jzt.kingpharmacist"
-    caps["appActivity"] = ".apploadpage.LoadingAc"
-    caps["autoGrantPermissions"] = "true"
-    caps["noSet"] = "true"
+class TestHysApp(object):
 
-    driver=webdriver.Remote("http://localhost:4723/wd/hub",caps)
+    @classmethod
+    def setUpClass(cls):
+        caps = {}
+        caps["platformName"] = "android"
+        caps["deviceName"] = "GT_New123"
+        caps["appPackage"] = "com.jzt.kingpharmacist"
+        caps["appActivity"] = ".apploadpage.LoadingAc"
+        caps["autoGrantPermissions"] = "true"
+        caps["noReset"] = "True"
+        cls.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
 
-    driver.implicitly_wait(10)
-    el1=driver.find_element_by_id("com.jzt.kingpharmacist:id/ll_skip").click()
-    el2=driver.find_element_by_id("com.jzt.kingpharmacist:id/fl_radio_profile").click()
-    el2_=driver.find_element_by_id("com.jzt.kingpharmacist:id/iv").click()
-    el3=driver.find_element_by_id("com.jzt.kingpharmacist:id/btn_login").click()
-    el4=driver.find_element_by_id("com.jzt.kingpharmacist:id/clet_phone").send_keys("15001106951")
-    el5=driver.find_element_by_id("com.jzt.kingpharmacist:id/clet_password").send_keys('123456')
-    el6=driver.find_element_by_id("com.jzt.kingpharmacist:id/btn_submit").click()
-    time.sleep(2)
-    el7=driver.find_element_by_id("com.jzt.kingpharmacist:id/ll_setting").click()
-    el8=driver.find_element_by_id("com.jzt.kingpharmacist:id/btn_logout").click()
-    time.sleep(2)
-    driver.quit()
+    def install_app(self):
+        pass
 
-def start_app():
-    pass
 
-def login_app():
-    pass
+    def start_app(self):
+        pass
 
-def search_goods():
-    pass
+    def login_app(self):
+        self.driver.implicitly_wait(10)
+        # el1=driver.find_element_by_id("com.jzt.kingpharmacist:id/ll_skip").click()
+        el2 = self.driver.find_element_by_id("com.jzt.kingpharmacist:id/fl_radio_profile").click()
+        # el2_=driver.find_element_by_id("com.jzt.kingpharmacist:id/iv").click()
+        el3 = self.driver.find_element_by_id("com.jzt.kingpharmacist:id/btn_login").click()
+        el4 = self.driver.find_element_by_id("com.jzt.kingpharmacist:id/clet_phone").send_keys("15001106951")
+        el5 = self.driver.find_element_by_id("com.jzt.kingpharmacist:id/clet_password").send_keys('123456')
+        el6 = self.driver.find_element_by_id("com.jzt.kingpharmacist:id/btn_submit").click()
 
-def place_order():
-    pass
+    def logout_app(self):
+        time.sleep(2)
+        el7 = self.driver.find_element_by_id("com.jzt.kingpharmacist:id/ll_setting").click()
+        el8 = self.driver.find_element_by_id("com.jzt.kingpharmacist:id/btn_logout").click()
 
-def cancel_order():
-    pass
+    def search_goods(self):
+        pass
 
-def test_hysapp():
-    install_app()
+    def place_order(self):
+        pass
+
+    def cancel_order(self):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        time.sleep(2)
+        cls.driver.quit()
+
+    def test_hysapp(self):
+        #self.install_app()
+        self.login_app()
+        self.logout_app()
