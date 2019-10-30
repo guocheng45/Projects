bfd7a4444c3453eeb8a5b77445fda5c0be18373f
guozhicheng46 <467563369@qq.com>
2019-10-30 20:20:34 +0800

    1030appium复习-1

--- page_object/testcases/test_hysapp.py
+++ page_object/testcases/test_hysapp.py
@@ -1,3 +1,5 @@
+#-*-coding:GBK -*-
+import action as action
 from appium import webdriver
 import time
 
@@ -13,16 +15,12 @@ class TestHysApp(object):
         caps["autoGrantPermissions"] = "true"
         caps["noReset"] = "True"
         cls.driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+        cls.driver.implicitly_wait(10)
 
     def install_app(self):
         pass
 
-
-    def start_app(self):
-        pass
-
     def login_app(self):
-        self.driver.implicitly_wait(10)
         # el1=driver.find_element_by_id("com.jzt.kingpharmacist:id/ll_skip").click()
         el2 = self.driver.find_element_by_id("com.jzt.kingpharmacist:id/fl_radio_profile").click()
         # el2_=driver.find_element_by_id("com.jzt.kingpharmacist:id/iv").click()
@@ -37,10 +35,24 @@ class TestHysApp(object):
         el8 = self.driver.find_element_by_id("com.jzt.kingpharmacist:id/btn_logout").click()
 
     def search_goods(self):
-        pass
+        sel1=self.driver.find_element_by_id("radio_home").click()
+        sel2=self.driver.find_element_by_id("qmy_main_search_ll").click()
+        sel3=self.driver.find_element_by_id("et_search").send_keys('¸ÐÃ°')
+        sel4=self.driver.find_element_by_id("bt_search")
+        self.driver.implicitly_wait(10)
+
+    def swipeUp_ui(self):
+        # x=self.driver.get_window_size()['width']
+        # y=self.driver.get_window_size()['height']
+        # self.driver.swipe(x*0.5,y*0.25,x*0.5,y*0.5)
+        #action=Touch
+        rect=self.driver.get_window_rect()
+        print("rect:",rect)
+        action.press()
 
     def place_order(self):
-        pass
+        #self.search_goods()
+        self.swipeUp_ui()
 
     def cancel_order(self):
         pass
@@ -52,5 +64,7 @@ class TestHysApp(object):
 
     def test_hysapp(self):
         #self.install_app()
-        self.login_app()
-        self.logout_app()
+        # self.login_app()
+        # self.logout_app()
+        # self.place_order()
+        self.swipeUp_ui()
