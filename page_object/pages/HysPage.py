0a7f6a4ad592d2747e4119ba30e3f4adfb1cb5e0
guocheng45 <467563369@qq.com>
2019-12-19 23:37:51 +0800

    12月19,家里appium家里修复-2

--- page_object/pages/HysPage.py
+++ page_object/pages/HysPage.py
@@ -44,7 +44,8 @@ class HysPage(HysBase):
         return self
 
     def judge_Searchresult(self,jr):
-        judge_result = self.driver.find_elements(By.XPATH, jr)
+        # judge_result = self.driver.find_elements(By.XPATH, jr)
+        judge_result = self.driver.find_elements(By.ID,"iv_buy")
         self.logger.info("=================", judge_result, '/n', type(judge_result))
         print("=================", judge_result, '/n', type(judge_result))
         return len(judge_result)
@@ -53,16 +54,15 @@ class HysPage(HysBase):
         self.find(By.ID,"iv_back").click()
         return self
 
-    def search_goodsO2O(self):
-        sel1=self.driver.find_element_by_id("function_img").click()
-        sel2=self.driver.find_element_by_id("et_search").click()
-        self.driver.implicitly_wait(10)
-        sel3=self.driver.find_element_by_id("et_search").send_keys('感冒')
-        sel4=self.driver.find_element_by_id("bt_search").click()
-        self.driver.implicitly_wait(5)
-        sel5=self.driver.find_element_by_id("tv_desc").click()
-        sel6=self.driver.find_element_by_id("tv_desc_2").click()
-        # self.driver.implicitly_wait(10)
+    def search_goodsO2O(self,kw):
+        # sel1=self.driver.find_element_by_id("function_img").click()
+        # sel2=self.driver.find_element_by_id("et_search").click()
+        # sel3=self.driver.find_element_by_id("et_search").send_keys('感冒')
+        # sel4=self.driver.find_element_by_id("bt_search").click()
+        # sel5=self.driver.find_element_by_id("tv_desc").click()
+        # sel6=self.driver.find_element_by_id("tv_desc_2").click()
+        self.loadSteps("../data/hys.yaml","search_goodsO2O",keys=kw)
+        return self
 
     def swipe_ui(self,act):
         time.sleep(3)
@@ -82,94 +82,95 @@ class HysPage(HysBase):
 
     def common_backtomainpage(self):
         # 下单不支付
-        cb1 = self.driver.find_element_by_id("iv_base_left").click()
-        cb2 = self.driver.find_element_by_id("tv_sure").click()
-        self.driver.implicitly_wait(10)
+        cb1 = self.find(By.ID,"iv_base_left").click()
+        cb2 = self.find(By.ID,"tv_sure").click()
         # 返回到首页
-        cb3 = self.driver.find_element_by_id("iv_base_left").click()
-        cb4 = self.driver.find_element_by_id("iv_back").click()
-        cb5 = self.driver.find_element_by_id("iv_back").click()
+        cb3 = self.find(By.ID,"iv_base_left").click()
+        cb4 = self.find(By.ID,"iv_back").click()
+        cb5 = self.find(By.ID,"iv_back").click()
 
-    def place_order_B2CCart(self):
-        self.search_goodsB2C()
+    def cart_goods_isSelected(self):
+        pel1 = self.find(By.XPATH,
+                         "//*[contains(@text,'马应龙 对乙酰氨基酚栓 0.125g*6s')]/../..//*[contains(@resource-id,'iv_buy')]").click()
+        pel2 = self.find(By.ID,"iv_cart").click()
+        return self
+
+    def place_order_B2CCart(self,kw):
+        self.search_goodsB2C(kw)
         self.swipe_ui("上拉")
-        pel1=self.driver.find_element_by_xpath("//*[contains(@text,'马应龙 对乙酰氨基酚栓 0.125g*6s')]/../..//*[contains(@resource-id,'iv_buy')]").click()
-        pel2=self.driver.find_element_by_id("iv_cart").click()
-        # self.driver.implicitly_wait(10)
-        # pel3=self.driver.find_element_by_id("rb_pharmacy_check").click()
-        # time.sleep(3)
-        # pel4=self.driver.find_element_by_xpath("//*[@text='马应龙 对乙酰氨基酚栓 0.125g*6s']/../..//*[contains(@resource-id,'rb_check')]").click()
-        time.sleep(3)
-        pel5=self.driver.find_element_by_id("tv_next").click()
-        pel6=self.driver.find_element_by_id("tv_submit_order").click()
+        pel1=self.find(By.XPATH,"//*[contains(@text,'马应龙 对乙酰氨基酚栓 0.125g*6s')]/../..//*[contains(@resource-id,'iv_buy')]").click()
+        pel2=self.find(By.ID,"iv_cart").click()
+        # 此处需判断购物车中的这个商品是否是选中状态？？？？
+        pel5=self.find(By.ID,"tv_next").click()
+        pel6=self.find(By.ID,"tv_submit_order").click()
         # 余额支付，不要支付了，不好退
-        # pel7=self.driver.find_element_by_id("cb_use_balance").click()
-        # pel8=self.driver.find_element_by_id("btn_add_addresss").click()
+        # pel7=self.find(By.ID,"cb_use_balance").click()
+        # pel8=self.find(By.ID,"btn_add_addresss").click()
         # self.driver.implicitly_wait(10)
-        # pel9=self.driver.find_element_by_id("tv_sure").click()
+        # pel9=self.find(By.ID,"tv_sure").click()
         # self.driver.implicitly_wait(10)
-        # pel10=self.driver.find_element_by_id("tv_order_detail").click()
+        # pel10=self.find(By.ID,"tv_order_detail").click()
         self.common_backtomainpage()
 
     def place_order_B2CBuyNow(self):
         self.search_goodsB2C()
         self.swipe_ui("上拉")
-        po1 = self.driver.find_element_by_xpath("//*[contains(@text,'马应龙 对乙酰氨基酚栓 0.125g*6s')]").click()
+        po1 = self.find(By.XPATH,"//*[contains(@text,'马应龙 对乙酰氨基酚栓 0.125g*6s')]").click()
         # po2 = self.driver.find_element_by_id("iv_img").click()
         time.sleep(3)
-        po3 = self.driver.find_element_by_id("tv_bottom_buy_now").click()
-        po4 = self.driver.find_element_by_id("tv_buy_now").click()
-        po5 = self.driver.find_element_by_id("tv_submit_order").click()
+        po3 = self.find(By.ID,"tv_bottom_buy_now").click()
+        po4 = self.find(By.ID,"tv_buy_now").click()
+        po5 = self.find(By.ID,"tv_submit_order").click()
 
         self.common_backtomainpage()
-        po6 = self.driver.find_element_by_id("iv_back").click()
+        po6 = self.find(By.ID,"iv_back").click()
 
     def place_order_B2CSale(self):
-        po1 = self.driver.find_element_by_id("ac_title").click()
-        # po1 = self.driver.find_element_by_xpath("//*[contains(@text,'马应龙 对乙酰氨基酚栓 0.125g*6s')]/../..//*[contains(@resource-id,'iv_buy')]").click()
+        po1 = self.find(By.ID,"ac_title").click()
+        # po1 = self.find(By.XPATH,"//*[contains(@text,'马应龙 对乙酰氨基酚栓 0.125g*6s')]/../..//*[contains(@resource-id,'iv_buy')]").click()
         # 多点两次页签2，怕点不中
-        po2 = self.driver.find_element_by_id("tv_flash_sale_status").click()
-        po2_1 = self.driver.find_element_by_id("tv_flash_sale_status").click()
+        po2 = self.find(By.ID,"tv_flash_sale_status").click()
+        po2_1 = self.find(By.ID,"tv_flash_sale_status").click()
         # 等待页面跳转
         time.sleep(3)
-        po3 = self.driver.find_element_by_id("tv_name").click()
-        po4 = self.driver.find_element_by_id("tv_bottom_buy_now").click()
-        po5 = self.driver.find_element_by_id("tv_buy_now").click()
-        po6 = self.driver.find_element_by_id("tv_submit_order").click()
+        po3 = self.find(By.ID,"tv_name").click()
+        po4 = self.find(By.ID,"tv_bottom_buy_now").click()
+        po5 = self.find(By.ID,"tv_buy_now").click()
+        po6 = self.find(By.ID,"tv_submit_order").click()
 
         self.common_backtomainpage()
-        po7 = self.driver.find_element_by_id("iv_base_left").click()
+        po7 = self.find(By.ID,"iv_base_left").click()
 
     def place_order_O2OCart(self):
         self.search_goodsO2O()
-        po1 = self.driver.find_element_by_xpath("//*[contains(@text,'东信 对乙酰氨基酚栓')]/../..//*[contains(@resource-id,'iv_buy')]").click()
-        po2 = self.driver.find_element_by_id("iv_cart").click()
+        po1 = self.find(By.XPATH,"//*[contains(@text,'东信 对乙酰氨基酚栓')]/../..//*[contains(@resource-id,'iv_buy')]").click()
+        po2 = self.find(By.ID,"iv_cart").click()
         time.sleep(3)
-        po3 = self.driver.find_element_by_id("tv_next").click()
-        po4 = self.driver.find_element_by_id("tv_submit_order").click()
+        po3 = self.find(By.ID,"tv_next").click()
+        po4 = self.find(By.ID,"tv_submit_order").click()
 
         self.common_backtomainpage()
-        po6 = self.driver.find_element_by_id("iv_back").click()
+        po6 = self.find(By.ID,"iv_back").click()
 
     def place_order_O2OBuyNow(self):
         self.search_goodsO2O()
-        po1 = self.driver.find_element_by_xpath("//*[contains(@text,'东信 对乙酰氨基酚栓')]").click()
-        # po2 = self.driver.find_element_by_id("iv_img").click()
+        po1 = self.find(By.XPATH,"//*[contains(@text,'东信 对乙酰氨基酚栓')]").click()
+        # po2 = self.find(By.ID,"iv_img").click()
         time.sleep(3)
-        po3 = self.driver.find_element_by_id("tv_bottom_buy_now").click()
-        po4 = self.driver.find_element_by_id("tv_buy_now").click()
-        po5 = self.driver.find_element_by_id("tv_submit_order").click()
+        po3 = self.find(By.ID,"tv_bottom_buy_now").click()
+        po4 = self.find(By.ID,"tv_buy_now").click()
+        po5 = self.find(By.ID,"tv_submit_order").click()
 
         self.common_backtomainpage()
-        po6 = self.driver.find_element_by_id("iv_back").click()
-        po7 = self.driver.find_element_by_id("iv_back").click()
+        po6 = self.find(By.ID,"iv_back").click()
+        po7 = self.find(By.ID,"iv_back").click()
 
     def cancel_order(self):
-        cel1 = self.driver.find_element_by_id("radio_profile").click()
-        cel2 = self.driver.find_element_by_id("img_my_orders").click()
-        cel3 = self.driver.find_elements_by_id("bt_cancel")[0].click()
-        cel4 = self.driver.find_element_by_id("iv_check").click()
-        cel5 = self.driver.find_element_by_id("bt_sure").click()
+        cel1 = self.find(By.ID,"radio_profile").click()
+        cel2 = self.find(By.ID,"img_my_orders").click()
+        cel3 = self.find(By.ID,"bt_cancel")[0].click()
+        cel4 = self.find(By.ID,"iv_check").click()
+        cel5 = self.find(By.ID,"bt_sure").click()
         time.sleep(3)
-        cel6 = self.driver.find_element_by_id("iv_base_left").click()
-        cel7 = self.driver.find_element_by_id("radio_home").click()
+        cel6 = self.find(By.ID,"iv_base_left").click()
+        cel7 = self.find(By.ID,"radio_home").click()
