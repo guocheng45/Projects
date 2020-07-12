df16943fcbc34341b9a0934b830da0860e2bb5ff
guocheng45 <467563369@qq.com>
2020-07-12 15:19:14 +0800

    2020.07.12,修改提交

---
+++ GTExercises/hogwarslearns/classdemo/hello_server.py
@@ -0,0 +1,33 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+@app.route("/request",methods=['post','get'])
+def hellp():
+    # 拿到request 参数
+    query = request.args
+    # 拿到request form
+    post = request.form
+    # 分别打印拿到的参数和form
+    return f"query:{query}\n"\
+            f"post:{post}"
+
+'''
+###简单服务器的搭建
+Administrator@19BXVNE26XTAXMJ MINGW64 /d/Projects/GTExercises/hogwarslearns/classdemo (master)
+$ export FLASK_APP=hello_server.py
+
+Administrator@19BXVNE26XTAXMJ MINGW64 /d/Projects/GTExercises/hogwarslearns/classdemo (master)
+$ flask run
+ * Serving Flask app "hello_server.py"
+ * Environment: production
+   WARNING: This is a development server. Do not use it in a production deployment.
+   Use a production WSGI server instead.
+ * Debug mode: off
+ * Running on http://127.0.0.1:5000/ (Press CTRL+C to quit)
+
+'''
--- Python_Basic/13random.py
+++ Python_Basic/13random.py
@@ -15,7 +15,8 @@ random_couse=random.choice(course)
 print(random_couse)
 
 # 大写小写字母和数字随机拼接为测试数据
-# 下面一行的注解：预留一个空的字符串'',.join方法后续拼接，  快速生成多个随机数的方法（x for i in range(8)）x非i 所以只是沿用了x的展示次数
+# 下面一行的注解：预留一个空的字符串'',.join方法后续拼接，  快速生成多个随机数的方法（x for i in range(8)）\
+# x非i 所以只是沿用了x的展示次数
 rad_str=''.join(random.choice(string.ascii_uppercase+string.ascii_lowercase+string.digits)for i in range(8))
 print(rad_str+'@163.com')
 
@@ -31,7 +32,8 @@ def use_random():
     print(ji)        # 随机获取0-100的奇数，概率一样
     print(random.choice('adadhasdhkahfkhfk'))   # 获取随机字符
     print(random.sample('dadsahdkahdusiwyiyriwyr',5))       # 随机生成指定数量的字符——输出：['a', 'y', 'a', 'i', 'r']
-    print(''.join(random.sample('dadsahdkahdusiwyiyriwyr', 5)))       # 随机生成指定数量的字符，并拼接成一个字符串，输出：ikwyy
+    print(''.join(random.sample('dadsahdkahdusiwyiyriwyr', 5)))       # 随机生成指定数量的字符，并拼接成一个字符串，\
+    # 输出：ikwyy
 
     print(random.sample('abcde', 5))        # 随机获取5个不同的随机值
     print(random.sample(range(100), 10))    # 在0-100中随机获取10个不同的数
